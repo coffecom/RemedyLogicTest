a25e0414ccf6fc8ce1aa2bf8995205e71e97cf91
evelina <coffecom@yandex.ru>
2021-10-22 03:58:02 +0300

    final django

--- segmentation_service/segmentation_app/torch_model.py
+++ segmentation_service/segmentation_app/torch_model.py
@@ -1,7 +1,9 @@
 import torch
 from torchvision.transforms import functional 
+from torchvision import transforms
 from pathlib import Path
 import h5py
+import io, base64
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,38 +17,63 @@ labels = {
 }
 picture_size = 256
 
-def resize_image_and_mask(image, mask, size = picture_size):
-  image = functional.resize(image, size)
-  mask = functional.resize(mask, size, transforms.InterpolationMode.NEAREST)
-  return image, mask
-
 def visualize_prediction(image, prediction):
     summs_along_channels = torch.sum(prediction, dim=[1,2])
     armax = torch.argmax(summs_along_channels).cpu().numpy()
-    pred_class = labels[armax + 1]
+    pred_label = labels[armax + 1]
     mask_transperent = prediction[armax].numpy().astype(np.float)
     mask_transperent[np.where(mask_transperent < 0.9)] = np.nan
 
-    plt.title("PREDICTED CLASS:" + pred_class)
+    plt.title("PREDICTED LABEL:" + pred_label)
     plt.imshow(image)
     plt.imshow(mask_transperent, cmap='jet', alpha = 0.7)
 
     plt.savefig("result.png")
 
-def prepare_image(file_path):
-    f = h5py.File(file_path, 'r')
-    image = np.array(f['cjdata']['image'])
+def transperent_mask(mask):
+    mask_transperent = mask.astype(np.float)
+    mask_transperent[np.where(mask_transperent < 0.9)] = np.nan
+    return mask_transperent
 
-    image = torch.Tensor([[image]])
-    print("SHAPE:", image.shape)
+def visualize_gt_pred(image, prediction, original_image, mask, label):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
+
+    gt_label = labels[label]
+    summs_along_channels = torch.sum(prediction, dim=[1,2])
+    armax = torch.argmax(summs_along_channels).cpu().numpy()
+    pred_label = labels[armax + 1]
+
+    ax1.set_title("Ground truth label:" + gt_label)
+    ax1.imshow(original_image)
+    ax1.imshow(transperent_mask(mask), cmap='jet', alpha = 0.7)
+    ax1.legend()
+
+    ax2.set_title("Predicted label:" + pred_label)
+    ax2.imshow(image)
+    ax2.imshow(transperent_mask(prediction[armax].numpy()), cmap='jet', alpha = 0.7)
+    ax2.legend()
+
+def prepare_image(raw_image):
+    image = torch.Tensor([[raw_image]])
     image = functional.resize(image, picture_size)
 
     return image.float()
 
 def predict(file_path):
-    image = prepare_image(file_path)
-    res = model.predict(image) 
-    visualize_prediction(image[0][0].numpy(), res[0]) # TODO add ground truth visualisation
-    
+    f = h5py.File(file_path, 'r')
+    image = np.array(f['cjdata']['image'])
 
-# predict(Path("D:\\programming\\remedylogic\\brainTumorDataPublic\\brainTumorDataPublic_767-1532\\767.mat"))
+    prepared_image = prepare_image(image)
+    res = model.predict(prepared_image) 
+
+    if ('tumorMask' in f['cjdata'] and 'label' in f['cjdata']):
+        mask = np.array(f['cjdata']['tumorMask'])
+        label = f['cjdata']['label']
+        visualize_gt_pred(prepared_image[0][0], res[0], image, mask, label[0][0])
+    else:
+        visualize_prediction(prepared_image[0][0], res[0]) 
+    
+    flike = io.BytesIO()
+    plt.savefig(flike)
+    b64 = base64.b64encode(flike.getvalue()).decode()
+    return b64
--- segmentation_service/segmentation_app/views.py
+++ segmentation_service/segmentation_app/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from .torch_model import predict
 
 
 def upload_file(request):
     if request.method == 'POST':
         upload = request.FILES['file']
-        fss = FileSystemStorage()
-        file = fss.save(upload.name, upload) #так сохраняет, ура. 
-        return HttpResponseRedirect('/segmentation/segmentation_success')  
+        
+        # fss = FileSystemStorage()
+        # file = fss.save(upload.name, upload) #так сохраняет, ура. 
+    
+        b64 = predict(upload.file)
+        # print("B64:" + b64)
+        return render(request, 'segmentation_success.html', context = {'prediction': b64})
     return render(request, 'upload.html')
 
-def segmentation_success(request):
-    images = [[1,1,1]]
-    return render(request, 'segmentation_success.html', {'images': images})
+def segmentation_success(request, prediction):
+    return render(request, 'segmentation_success.html', {'prediction': prediction})
